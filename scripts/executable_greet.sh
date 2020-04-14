2c0d15e29d8cbc2b788fe3f821e71d993d8c8389
lowercasename <raphaelkabo@gmail.com>
2020-04-14 08:42:03 +0100

    Update scripts/executable_greet.sh

--- scripts/executable_greet.sh
+++ scripts/executable_greet.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-h=`date +%H`
+hour=`date +%H`
 
-if (( h >= 6 && h < 12)); then
+if [ $hour -ge 6 -a $hour -lt 12 ]; then
   echo Good morning, Raphael \${font Font Awesome 5 Pro:size=14:style=Solid}\$font
-elif (( h >= 12 && h < 18)); then
+elif [ $hour -ge 12 -a $hour -lt 18 ]; then
   echo Good afternoon, Raphael \${font Font Awesome 5 Pro:size=14:style=Solid}\$font
 else
   echo Good evening, Raphael \${font Font Awesome 5 Pro:size=14:style=Solid}\$font
